5c26c719ed9b62449a2e5793572bf5f81dd0c098
jaeyoung74 <wodud74@naver.com>
2026-01-29 22:17:51 +0900

    0126데일리미션

---
+++ pandas/daily_mission/0126/ex1.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+nums = [i for i in range(1,11)]
+print(nums)
+
+#series 데이터 설정방법
+SeriesNums = pd.Series(nums)
+print(SeriesNums)
+
+#평균값 출력
+print(SeriesNums.mean())
+
+#분석값 출력
+print(SeriesNums.describe())
+
+#DataFrame 생성방법
+nums1 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+dfNums1 = pd.DataFrame(nums1)
+print(dfNums1)
+print(dfNums1[0])
